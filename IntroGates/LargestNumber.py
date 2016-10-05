4b8abb1fcb2c3ef477f9aeb39b7089b424534805
Cathie So <socathie@users.noreply.github.com>
2016-10-04 20:52:10 -0700

    Update LargestNumber.py

--- IntroGates/LargestNumber.py
+++ IntroGates/LargestNumber.py
@@ -1,3 +1,15 @@
+#Given an integer n, return the largest number that contains exactly n digits.
+#Example
+#For n = 2, the output should be
+#largestNumber(n) = 99.
+#Input/Output
+#[time limit] 4000ms (py)
+#[input] integer n
+#Constraints:
+#1 ≤ n ≤ 7.
+#[output] integer
+#The largest integer of length n.
+
 def largestNumber(n):
     temp = 0
     for x in range(0,n):
