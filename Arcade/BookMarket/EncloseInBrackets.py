ddf6f165c2bf2a6b719e7d24c40d9353774c2c21
Cathie So <socathie@users.noreply.github.com>
2016-10-04 21:32:19 -0700

    Create EncloseInBrackets.py

---
+++ Arcade/BookMarket/EncloseInBrackets.py
@@ -0,0 +1,17 @@
+#Given a string, enclose it in round brackets.
+
+#Example
+#For inputString = "abacaba", the output should be
+#encloseInBrackets(inputString) = "(abacaba)".
+
+#Input/Output
+#[time limit] 4000ms (py)
+
+#[input] string inputString
+#Constraints:
+#0 ≤ inputString.length ≤ 10.
+
+#[output] string
+
+def encloseInBrackets(inputString):
+    return "("+inputString+")"
