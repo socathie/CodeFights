944e9eed0931e023410b6e33e7265bc3ecb0423b
Cathie So <socathie@users.noreply.github.com>
2016-10-05 23:54:42 -0700

    Create AreSimilar.py

---
+++ Arcade/WellOfIntegration/AreSimilar.py
@@ -0,0 +1,50 @@
+'''Two arrays are called similar if one can be obtained from another by swapping at most one pair of elements.
+
+Given two arrays, check whether they are similar.
+
+Example
+
+For A = [1, 2, 3] and B = [1, 2, 3], the output should be
+areSimilar(A, B) = true;
+For A = [1, 2, 3] and B = [2, 1, 3], the output should be
+areSimilar(A, B) = true;
+For A = [1, 2, 2] and B = [2, 1, 1], the output should be
+areSimilar(A, B) = false.
+Input/Output
+
+[time limit] 4000ms (py)
+[input] array.integer A
+
+Array of integers.
+
+Constraints:
+3 ≤ A.length ≤ 105,
+1 ≤ A[i] ≤ 1000.
+
+[input] array.integer B
+
+Array of integers of the same length as A.
+
+Constraints:
+B.length = A.length,
+1 ≤ B[i] ≤ 1000.
+
+[output] boolean
+
+true if A and B are similar, false otherwise.'''
+
+def areSimilar(A, B):
+    if A==B:
+        return True
+    
+    for i in range(0,len(A)):
+        for j in range(i,len(A)):
+            temp = A[i]
+            A[i] = A[j]
+            A[j] = temp
+            if A == B:
+                return True
+            A[j] = A[i]
+            A[i] = temp
+            
+    return False
