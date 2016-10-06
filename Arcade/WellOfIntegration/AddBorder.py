6d930cb8ece66488cc6608e1a92c32be99c64aa6
Cathie So <socathie@users.noreply.github.com>
2016-10-05 23:51:03 -0700

    Create AddBorder.py

---
+++ Arcade/WellOfIntegration/AddBorder.py
@@ -0,0 +1,41 @@
+'''Given a a rectangular matrix of characters, add a border of asterisks(*) to it.
+
+Example
+
+For
+
+picture = ["abc",
+           "ded"]
+the output should be
+
+addBorder(picture) = ["*****",
+                      "*abc*",
+                      "*ded*",
+                      "*****"]
+Input/Output
+
+[time limit] 4000ms (py)
+[input] array.string picture
+
+A non-empty array of non-empty equal-length strings.
+
+Constraints:
+1 ≤ picture.length ≤ 5,
+1 ≤ picture[i].length ≤ 5.
+
+[output] array.string
+
+The same matrix of characters, framed with a border of asterisks of width 1.'''
+
+def addBorder(picture):
+    output = []
+    border = ""
+    for i in range(0,len(picture[0])+2):
+        border += "*"
+    output.append(border)
+    for i in range(0,len(picture)):
+        output.append("*"+picture[i]+"*")
+    
+    output.append(border)
+    
+    return output
