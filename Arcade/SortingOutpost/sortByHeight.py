3a99ece461074141cad125a21e2e605291c2c473
Cathie So <socathie@users.noreply.github.com>
2016-10-14 00:37:38 -0700

    Create sortByHeight.py

---
+++ Arcade/SortingOutpost/sortByHeight.py
@@ -0,0 +1,32 @@
+def sortByHeight(a):
+    b = sorted([i for i in a if i!=-1])
+    j = 0
+    for i in range(len(a)):
+        if a[i] == -1:
+            pass
+        else:
+            a[i]=b[j]
+            j+=1
+    return a
+    
+'''Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by their heights in a non-descending order without moving the trees.
+
+Example
+
+For a = [-1, 150, 190, 170, -1, -1, 160, 180], the output should be
+sortByHeight(a) = [-1, 150, 160, 170, -1, -1, 180, 190].
+
+Input/Output
+
+[time limit] 4000ms (py)
+[input] array.integer a
+
+If a[i] = -1, then the ith position is occupied by a tree. Otherwise a[i] is the height of a person standing in the ith position.
+
+Constraints:
+5 ≤ a.length ≤ 15,
+-1 ≤ a[i] ≤ 200.
+
+[output] array.integer
+
+Sorted array a with all the trees untouched.'''
