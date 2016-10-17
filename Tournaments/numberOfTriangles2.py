3c2b21461bb0b1d111ef8a9eedbde735acdf16a7
Cathie So <socathie@users.noreply.github.com>
2016-10-17 01:41:34 -0700

    Create numberOfTriangles2.py

---
+++ Tournaments/numberOfTriangles2.py
@@ -0,0 +1,10 @@
+def numberOfTriangles2(sticks):
+    cnt = 0
+    sticks = sorted(sticks,reverse = True)
+    for i in range(len(sticks)):
+        for j in range(i+1,len(sticks)):
+            for k in range(j+1,len(sticks)):
+                if sticks[k]+sticks[j]>sticks[i]:
+                    cnt += 1
+                    print sticks[i],sticks[j],sticks[k]
+    return cnt
