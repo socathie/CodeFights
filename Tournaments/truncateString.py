53f8e319b7d06b2f96eb68f9c8068e62db4ed5f3
Cathie So <socathie@users.noreply.github.com>
2016-10-18 00:07:30 -0700

    Update truncateString.py

--- Tournaments/truncateString.py
+++ Tournaments/truncateString.py
@@ -7,3 +7,28 @@ def truncateString(s):
         elif (int(s[0])+int(s[-1]))%3==0:
             s = s[1:-1]
     return s
+
+##########
+
+def truncateString(s):
+
+    def truncate(l, r):
+        if l >= r:
+            return ''
+        newL = l
+        newR = r
+        left = ord(s[l]) - ord('0')
+        right = ord(s[r - 1]) - ord('0')
+        if left % 3 == 0:
+            newL += 1
+        elif right % 3 == 0:
+            newR -= 1
+        elif (left + right) % 3 == 0:
+            newL += 1
+            newR -= 1
+        else:
+            return s[l : r]
+
+        return truncate(newL, newR)
+
+    return truncate(0, len(s))
