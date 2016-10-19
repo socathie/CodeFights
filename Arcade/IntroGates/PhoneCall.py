0763bd3cb2226865024ceb26f0691105dd46ba86
Cathie So <socathie@users.noreply.github.com>
2016-10-18 22:42:32 -0700

    Update PhoneCall.py

--- Arcade/IntroGates/PhoneCall.py
+++ Arcade/IntroGates/PhoneCall.py
@@ -54,3 +54,15 @@ def phoneCall(min1, min2_10, min11, S):
             temp += 1
         
     return temp
+
+##########
+
+def phoneCall(min1, min2_10, min11, s):
+
+    if s < min1:
+        return 0
+    for i in range(2, 11):
+        if s < min1 + min2_10 * (i - 1):
+            return i - 1
+    return (s - min1 - min2_10 * 9.0) //min11 + 10
+
