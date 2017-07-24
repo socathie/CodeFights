189d8043a0fc0eb44cd93800eb4d1f7726ec787c
Jeb Sumeracki <jebsumer@yahoo.com>
2017-07-24 09:18:26 -0500

    Update rounders.py
    There was a typo

--- Arcade/LoopTunnel/Rounders.py
+++ Arcade/LoopTunnel/Rounders.py
@@ -27,7 +27,7 @@
 def rounders(value):
     digit = 1
     if value < 10:
-        return b
+        return value
     if value%pow(10,digit)>=5:
         value += pow(10,digit)
     value -= (value%pow(10,digit))
